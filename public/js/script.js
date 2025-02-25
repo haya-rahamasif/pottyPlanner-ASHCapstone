14423e6eb73c17fa8860809f61eb7d09b37ba5f1
TonyShen7 <159183430+TonyShen7@users.noreply.github.com>
2025-02-25 14:02:25 +0000

    added buttons

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,38 @@
 console.log("js loaded!")
+const ret = document.getElementById("timer");
+const startBtn = document.querySelector("#start-timer");
+let counter = 0;
+let interval;
+
+function stop() {
+  clearInterval(interval);
+  startBtn.disabled = false;
+}
+
+function convertSec(cnt) {
+  let sec = cnt % 60;
+  let min = Math.floor(cnt / 60);
+  if (sec < 10) {
+    if (min < 10) {
+      return "0" + min + ":0" + sec;
+    } else {
+      return min + ":0" + sec;
+    }
+  } else if ((min < 10) && (sec >= 10)) {
+    return "0" + min + ":" + sec;
+  } else {
+    return min + ":" + sec;
+  }
+}
+
+function start() {
+  startBtn.disabled = true;
+  interval = setInterval(function() {
+    ret.innerHTML = convertSec(counter++); // timer start counting here...
+  }, 1000);
+}
+function reset() {
+  startBtn.disabled = false;
+  counter = 0; // Reset the counter to zero
+  ret.innerHTML = convertSec(counter);
+}
