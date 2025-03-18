27e365d1ed5d4056e30ac1ec5a8d8c69181c2305
TonyShen7 <159183430+TonyShen7@users.noreply.github.com>
2025-03-18 13:02:38 +0000

    saves

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-    console.log('Server started on port 3000')
+app.listen(3001, () => {
+    console.log('Server started on port 3001')
 })
 
 const dbURL = 'mongodb+srv://hayarahamasif:preach-immature-mouthful-smoky@pottyplannerdb.jg0o8.mongodb.net/?retryWrites=true&w=majority&appName=pottyPlannerDB'
