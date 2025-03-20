c8f14b24f8ff35bdb002f5705fd2989b66c1ff5c
haya-rahamasif <haya.raham-asif@stu.ocsb.ca>
2025-03-20 09:42:33 -0400

    adding time data to db (working)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import ejs from 'ejs'
 import mongoose from 'mongoose'
 
-import User from './models/users.js'
+import Student from './models/students.js'
 import path from 'path';
 const __dirname = path.resolve();
 
@@ -20,34 +20,37 @@ app.get('/', (req, res) => {
     res.render('../views/index.ejs');
 });
 
-// Start the server
-app.listen(3001, () => {
-    console.log('Server started on port 3001')
+app.get('/timestamp', (req, res) => {
+    res.render("timestamp route")
 })
 
-const dbURL = 'mongodb+srv://hayarahamasif:preach-immature-mouthful-smoky@pottyplannerdb.jg0o8.mongodb.net/?retryWrites=true&w=majority&appName=pottyPlannerDB'
+app.use(express.json())
+app.post('/timestamp', (req, res) => {
+    // creates a instance of the user schema to make a new field (row of data) to add to the database
+    let time = req.body.data
+    let entry1 = new Student ({
+        studentName:"john smith",
+        studentID:"0924",
+        absenceP1:[time],
+        absenceP2:["0"],
+        absenceP3:["0"],
+        absenceP4:["0"]
+    })
 
-// creates a instance of the user schema to make a new field (row of data) to add to the database
-let entry1 = new User ({
-    name: 'sam',
-    email: 'def@hij.com',
-    password: 'hello world'
-})
-/*
-entry1 
+    entry1 
     .save()
     .then((doc) => {
         console.log(doc)
     })
     .catch((err) => {
         console.log(err)
-    })*/
+    })
 
-mongoose 
+    mongoose 
     .connect(dbURL) // connects to database
     .then((result) => {
         console.log('Connected to MongoDB')
-        User.find() // finds all documents (columns) in the collection (table) and returns them. usually you can also specify a filter of some sort to only return specific data
+        User.find({studentID: '0'}) // finds all documents (columns) in the collection (table) and returns them. usually you can also specify a filter of some sort to only return specific data
         .then((doc) => { // doc is the result that is returned from the .find() method
             console.log(doc)
         })
@@ -58,13 +61,28 @@ mongoose
     .catch((err) => {
         console.error('could not connect to mongodb: ', err)
     })
+    
+})
 
-app.get('/', (req, res) => { // this code will only run if it receives a web request from the client side
-    User.find({name: "john smith"})
-    .then((result) => {
-        console.log(` result -> ${result}`)
+// Start the server
+app.listen(3001, () => {
+    console.log('Server started on port 3001')
+})
+
+const dbURL = 'mongodb+srv://hayarahamasif:preach-immature-mouthful-smoky@pottyplannerdb.jg0o8.mongodb.net/?retryWrites=true&w=majority&appName=pottyPlannerDB'
+
+
+/*
+entry1 
+    .save()
+    .then((doc) => {
+        console.log(doc)
     })
     .catch((err) => {
-        console.error(err)
-    })
+        console.log(err)
+    })*/
+
+
+
+app.get('/', (req, res) => { // this code will only run if it receives a web request from the client side
 })
