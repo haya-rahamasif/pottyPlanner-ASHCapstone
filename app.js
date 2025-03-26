d3129e8962b99ecab86ad93fe551e567715466dc
haya raham asif <haya.raham-asif@stu.ocsb.ca>
2025-03-26 20:22:41 +0000

    time classification possibly working

--- app.js
+++ app.js
@@ -29,7 +29,27 @@ app.post('/timestamp', (req, res) => {
     // creates a instance of the student schema to make a new field (row of data) to add to the database
     let time = req.body.data
     console.log(time)
-    /*let entry1 = new Student ({
+    let startTime = time[0]
+
+    if (startTime[3] >= 8 && startTime[4] >=20) {
+        if (startTime[3] <= 9 && startTime[4] <= 45){
+            console.log('p1 absence')
+        }
+    } else if (startTime[3] >= 9 && startTime[4] <= 55){
+        if (startTime[4] >=5 && startTime[3] <= 11){
+            console.log('p2 absence')
+        }
+    } else if (startTime[3] >= 11 && startTime[4] <= 55){
+        if (startTime[4] <=10 && startTime[3] <= 1){
+            console.log('p3 absence')
+        }
+    } else if (startTime[3] >= 1 && startTime[4] <= 15){
+        if (startTime[3] >=2 && startTime[4] <= 30){
+            console.log('p4 absence')
+        }
+    }
+
+    /*let entry = new Student ({
         studentName:"john smith",
         studentID:"0924",
         absenceP1:[time],
@@ -38,22 +58,22 @@ app.post('/timestamp', (req, res) => {
         absenceP4:["0"]
     })
 
-    entry1 
+    entry 
     .save()
     .then((doc) => {
         console.log(doc)
     })
     .catch((err) => {
         console.log(err)
-    })*/
+    })
 
     mongoose 
     .connect(dbURL) // connects to database
     .then((result) => {
         console.log('Connected to MongoDB')
-        Student.find({studentID: '0'}) // finds all documents (columns) in the collection (table) and returns them. usually you can also specify a filter of some sort to only return specific data
+        Student.find({studentID: '0924'}) // finds all documents (columns) in the collection (table) and returns them. usually you can also specify a filter of some sort to only return specific data
         .then((doc) => { // doc is the result that is returned from the .find() method
-            console.log(doc)
+            doc.
         })
         .catch((err) => {
             console.log(err)
@@ -61,7 +81,7 @@ app.post('/timestamp', (req, res) => {
     })
     .catch((err) => {
         console.error('could not connect to mongodb: ', err)
-    })
+    })*/
     
 })
 
